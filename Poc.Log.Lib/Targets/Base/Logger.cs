e053fec5f73af51e22300b4cf4e8e54107d4f0ed
Fernando Spelling <f.l.spelling@gmail.com>
2020-04-20 12:22:52 -0300

    .

--- Poc.Log.Lib/Targets/Base/Logger.cs
+++ Poc.Log.Lib/Targets/Base/Logger.cs
@@ -3,19 +3,13 @@ using System.Collections.Generic;
 
 namespace Poc.Log.Lib.Targets.Base
 {
-    public class Logger : Interfaces.ILogger
+    public class Logger
     {
         protected NLog.Logger _log;
 
-        public Logger()
-        {
-            _log = LogManager.GetLogger();
-        }
+        public Logger() => _log = LogManager.GetLogger();
 
-        public virtual void Gravar(LogType logType, string message, Dictionary<string, object> objects) =>
-            _log.Log(GerarLogEventInfo(logType, message, objects));
-
-        private NLog.LogEventInfo GerarLogEventInfo(LogType logType, string message, Dictionary<string, object> objects) =>
+        protected NLog.LogEventInfo GerarLogEventInfo(LogType logType, string message, Dictionary<string, object> objects) =>
             new NLog.LogEventInfo();
     }
 }
--- Poc.Log.Lib/Targets/LoggerMongo.cs
+++ Poc.Log.Lib/Targets/LoggerMongo.cs
@@ -1,6 +1,11 @@
-﻿namespace Poc.Log.Lib.Targets
+﻿using Poc.Log.Lib.Enums;
+using System.Collections.Generic;
+
+namespace Poc.Log.Lib.Targets
 {
-    public class LoggerMongo : Base.Logger
+    public class LoggerMongo : Base.Logger, Interfaces.ILogger
     {
+        public virtual void Gravar(LogType logType, string message, Dictionary<string, object> objects) =>
+            _log.Log(GerarLogEventInfo(logType, message, objects));
     }
 }
